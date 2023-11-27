ebaf019896c81d57be5f5b49fdea66c39e45331c
bddwyx <bddwyx@sjtu.edu.cn>
2023-11-27 21:33:49 +0800

    feat(BSP): Cubemx生成Robomaster C板代码
    在main函数中增加Setup与Loop两个接口

---
+++ Bsp/Robomaster_C/Robomaster_C.h
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2023.
+ * IWIN-FINS Lab, Shanghai Jiao Tong University, Shanghai, China.
+ * All rights reserved.
+ ******************************************************************************/
+
+#ifndef FINEMOTE_ROBOMASTER_C_H
+#define FINEMOTE_ROBOMASTER_C_H
+
+#include "main.h"
+#include "adc.h"
+#include "can.h"
+#include "dma.h"
+#include "i2c.h"
+#include "iwdg.h"
+#include "spi.h"
+#include "tim.h"
+#include "usart.h"
+#include "gpio.h"
+
+#define Serial_Host huart6
+#define Serial_RS4851 huart1
+
+#define SPI_BMI088 hspi1 /** todo */
+
+#endif //FINEMOTE_ROBOMASTER_C_H
