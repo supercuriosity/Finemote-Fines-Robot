49e9f82629ca763bad7084c3560bae94731c0a58
AcetoneChen <chenbingtong@sjtu.edu.cn>
2023-11-27 20:52:34 +0800

    feat(BSP): 板级头文件MC_Board

---
+++ Bsp/MC_Board/MC_Board.h
@@ -0,0 +1,16 @@
+#ifndef FINEMOTE_MC_BOARD_H
+#define FINEMOTE_MC_BOARD_H
+
+#include "main.h"
+#include "adc.h"
+#include "can.h"
+#include "i2c.h"
+#include "spi.h"
+#include "tim.h"
+#include "usart.h"
+#include "gpio.h"
+
+
+#define SerialHost huart4
+#define SerialUser huart5
+#endif //FINEMOTE_MC_BOARD_H
