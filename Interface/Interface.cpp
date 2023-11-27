ac0021ecab3cc9a9457da8890eecca284d91f968
bddwyx <bddwyx@sjtu.edu.cn>
2023-11-27 20:32:59 +0800

    feat(Interface): 主函数C++接口
    在main函数中增加Setup与Loop两个接口

---
+++ Interface/Interface.cpp
@@ -0,0 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2023.
+ * IWIN-FINS Lab, Shanghai Jiao Tong University, Shanghai, China.
+ * All rights reserved.
+ ******************************************************************************/
+
+#include "Interface.h"
+
+void Setup() {
+
+}
+
+void Loop() {
+
+}
---
+++ Interface/Interface.h
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2023.
+ * IWIN-FINS Lab, Shanghai Jiao Tong University, Shanghai, China.
+ * All rights reserved.
+ ******************************************************************************/
+
+#ifndef FINEMOTE_INTERFACE_H
+#define FINEMOTE_INTERFACE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    void Setup();
+    void Loop();
+#ifdef __cplusplus
+};
+#endif
+
+#endif //FINEMOTE_INTERFACE_H
