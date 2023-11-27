a0e9b50b75ebc41632688fcab36346d9e7b3d76a
bddwyx <bddwyx@sjtu.edu.cn>
2023-11-27 23:39:35 +0800

    feat(Devices): 增加设备BMI088
    博世IMU

---
+++ Devices/Sensors/BMI088.h
@@ -0,0 +1,12 @@
+/*******************************************************************************
+ * Copyright (c) 2023.
+ * IWIN-FINS Lab, Shanghai Jiao Tong University, Shanghai, China.
+ * All rights reserved.
+ ******************************************************************************/
+
+#ifndef FINEMOTE_BMI088_H
+#define FINEMOTE_BMI088_H
+
+
+
+#endif //FINEMOTE_BMI088_H
