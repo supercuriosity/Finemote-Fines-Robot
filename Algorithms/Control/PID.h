2297e80d8ac3b948ae3775fcb9b9dd84ec22e5eb
bddwyx <bddwyx@sjtu.edu.cn>
2023-11-28 17:44:53 +0800

    feat(Algorithms): 添加支持功能
    控制类（PID）、基础类（哈希Map）

---
+++ Algorithms/Control/PID.h
@@ -0,0 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2023.
+ * IWIN-FINS Lab, Shanghai Jiao Tong University, Shanghai, China.
+ * All rights reserved.
+ ******************************************************************************/
+
+#ifndef FINEMOTE_PID_H
+#define FINEMOTE_PID_H
+
+#endif //FINEMOTE_PID_H
---
+++ Algorithms/Wheels/HashMap.cpp
@@ -0,0 +1,7 @@
+/*******************************************************************************
+ * Copyright (c) 2023.
+ * IWIN-FINS Lab, Shanghai Jiao Tong University, Shanghai, China.
+ * All rights reserved.
+ ******************************************************************************/
+
+#include "HashMap.h"
