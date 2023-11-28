0ed9d13095bd46acf338d9d4d3445a6e4bc63caf
bddwyx <bddwyx@sjtu.edu.cn>
2023-11-28 17:45:29 +0800

    feat(Components): 添加支持功能
    底盘、机械臂

---
+++ Components/Chassis.h
@@ -0,0 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2023.
+ * IWIN-FINS Lab, Shanghai Jiao Tong University, Shanghai, China.
+ * All rights reserved.
+ ******************************************************************************/
+
+#ifndef FINEMOTE_CHASSIS_H
+#define FINEMOTE_CHASSIS_H
+
+#endif //FINEMOTE_CHASSIS_H
---
+++ Components/Manipulator.h
@@ -0,0 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2023.
+ * IWIN-FINS Lab, Shanghai Jiao Tong University, Shanghai, China.
+ * All rights reserved.
+ ******************************************************************************/
+
+#ifndef FINEMOTE_MANIPULATOR_H
+#define FINEMOTE_MANIPULATOR_H
+
+#endif //FINEMOTE_MANIPULATOR_H
