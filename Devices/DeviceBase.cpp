f4d53296e3bbcdd15c44e63fec09683b9a69e821
majiayue2020 <59561366+majiayue2020@users.noreply.github.com>
2024-01-15 13:45:06 +0800

    feat(Devices)：设备基类分频执行功能

--- Devices/DeviceBase.cpp
+++ Devices/DeviceBase.cpp
@@ -6,17 +6,28 @@
 
 #include "DeviceBase.h"
 
+std::list<DeviceBase*> DeviceBase::deviceList={};
 
 void DeviceBase::DevicesHandle() {
+    auto tmp = deviceList.size();
     for (DeviceBase* devicePtr :deviceList){
-        devicePtr->Handle();
+        if (++(devicePtr->cnt)>= devicePtr->divisionFactor) {
+            devicePtr->Handle();
+            devicePtr->cnt = 0;
+        }
     }
 }
 
 DeviceBase::DeviceBase() {
+    auto tmp = deviceList.size();
     deviceList.push_back(this);
 }
 
 DeviceBase::~DeviceBase() {
+    auto tmp = deviceList.size();
     deviceList.remove(this);
+}
+
+void DeviceBase::SetDivisionFactor(uint32_t divisionFactor) {
+    DeviceBase::divisionFactor = divisionFactor;
 }
--- Devices/DeviceBase.h
+++ Devices/DeviceBase.h
@@ -16,9 +16,16 @@ public:
 
     DeviceBase();
     ~DeviceBase();
+    /**
+     * 设置分频系数，使设备的执行频率动态可调
+     * @param divisionFactor
+     */
+    void SetDivisionFactor(uint32_t divisionFactor);
 private:
-    inline static std::list<DeviceBase*> deviceList = {};
+    uint32_t cnt{0};
+    uint32_t divisionFactor{1};
 
+    static std::list<DeviceBase*> deviceList;
 };
 
 
