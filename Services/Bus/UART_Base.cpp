9a8a75b477481c7f731aa3d3c10cb8b6decdbea0
huigg296 <wangjinghui@sjtu.edu.cn>
2024-02-20 11:32:58 +0800

    feat(Services): UART基类实现

--- Services/Bus/UART_Base.cpp
+++ Services/Bus/UART_Base.cpp
@@ -6,4 +6,28 @@
 
 # include "UART_Base.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
+// 发送完成中断回调函数
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart) {
+    UART_Bus<6>::GetInstance().CallbackHandle(UART_Bus<6>::Callback_e::WRITE);
+}
+// 接收中断回调函数
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+    UART_Bus<6>::GetInstance().CallbackHandle(UART_Bus<6>::Callback_e::READ);
+}
+
+void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size) {
+    UART_Bus<6>::GetInstance().CallbackHandle(UART_Bus<6>::Callback_e::READ);
+}
+
+// 出错中断回调函数
+void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart) {
+    UART_Bus<6>::GetInstance().CallbackHandle(UART_Bus<6>::Callback_e::ERROR_CALL);
+}
+
+#ifdef __cplusplus
+}
+#endif
