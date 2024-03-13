4cd0f827b2e46d2e1657f229ea13f8835ec26467
majiayue2020 <59561366+majiayue2020@users.noreply.github.com>
2024-03-13 21:31:41 +0800

    Refactor(Services)：UART总线类支持多各串口

--- Services/Bus/UART_Base.cpp
+++ Services/Bus/UART_Base.cpp
@@ -14,20 +14,22 @@ extern "C" {
 
 // 发送完成中断回调函数
 void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart) {
-    UART_Bus<6>::GetInstance().CallbackHandle(UART_Bus<6>::Callback_e::WRITE);
+    GetUartHandle_BusMap()[huart]->CallbackHandle(UART_Base::Callback_e::WRITE);
+    //UART_Bus<0>::GetInstance().CallbackHandle(UART_Bus<0>::Callback_e::WRITE);
 }
 // 接收中断回调函数
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
-    UART_Bus<6>::GetInstance().CallbackHandle(UART_Bus<6>::Callback_e::READ);
+    GetUartHandle_BusMap()[huart]->CallbackHandle(UART_Base::Callback_e::READ);
+    //UART_Bus<0>::GetInstance().CallbackHandle(UART_Bus<0>::Callback_e::READ);
 }
 
 void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size) {
-    UART_Bus<6>::GetInstance().CallbackHandle(UART_Bus<6>::Callback_e::READ);
+    GetUartHandle_BusMap()[huart]->CallbackHandle(UART_Base::Callback_e::READ);
 }
 
 // 出错中断回调函数
 void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart) {
-    UART_Bus<6>::GetInstance().CallbackHandle(UART_Bus<6>::Callback_e::ERROR_CALL);
+    GetUartHandle_BusMap()[huart]->CallbackHandle(UART_Base::Callback_e::ERROR_CALL);
 }
 
 #ifdef __cplusplus
