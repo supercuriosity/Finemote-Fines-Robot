c9cc4ce6f26669ea43e2185c6e446d9c02705d56
majiayue2020 <59561366+majiayue2020@users.noreply.github.com>
2024-04-05 20:27:41 +0800

    Refactor(Devices)：设备列表改为反向遍历

--- Devices/DeviceBase.cpp
+++ Devices/DeviceBase.cpp
@@ -13,21 +13,21 @@ void DeviceBase::DevicesHandle() {
     //auto tmp = getDeviceList().size()
     static uint32_t stamp = HAL_GetTick();
     static uint32_t cnt = 0;
-    uint32_t diff = HAL_GetTick()-stamp;
+    uint32_t diff = HAL_GetTick() - stamp;
     cnt++;
-    if (diff>=1000){
+    if (diff >= 1000) {
         baseFre = cnt;
         stamp = HAL_GetTick();
         cnt = 0;
     }
-    for (DeviceBase* devicePtr :getDeviceList()){
-        if (++(devicePtr->cnt)>= devicePtr->divisionFactor) {
+    for (auto rit = getDeviceList().rbegin(); rit != getDeviceList().rend(); ++rit) {//反向遍历
+        auto devicePtr = *rit;
+        if (++(devicePtr->cnt) >= devicePtr->divisionFactor) {
             devicePtr->Handle();
             devicePtr->cnt = 0;
         }
     }
 }
-
 DeviceBase::DeviceBase() {
     getDeviceList().push_back(this);
 	auto tmp = getDeviceList().size();
