e04cd90b5674950a0338ec226c480fbda2fade1d
Xy688yyds <xy.2861981637@sjtu.edu.cn>
2024-05-15 17:28:55 +0800

    Fix(Components):底盘类车轮解算修改

--- Components/Chassis.cpp
+++ Components/Chassis.cpp
@@ -43,58 +43,19 @@ void Chassis::WheelsSpeedCalc(float fbVelocity, float lrVelocity, float rtVeloci
     D = vy - w * WIDTH / 2;
 
     //计算四个轮子角度，单位：度
-    RFRAngle = atan2(B, C) * 180 / PI;
-    RFLAngle = atan2(B, D) * 180 / PI;
-    RBLAngle = atan2(A, D) * 180 / PI;
-    RBRAngle = atan2(A, C) * 180 / PI;
+    RFRAngle = atan2(B, D) * 180 / PI;
+    RFLAngle = atan2(B, C) * 180 / PI;
+    RBLAngle = (atan2(A, D) + PI) * 180 / PI;
+    RBRAngle = (atan2(A, C) + PI) * 180 / PI;
 
     //计算四个轮子线速度，单位：度/s
-
-    ClassisSpeed[0] = -sqrt(B * B + C * C) / (WHEEL_DIAMETER * PI) * 360;//右前轮
-    ClassisSpeed[1] = sqrt(B * B + D * D) / (WHEEL_DIAMETER * PI) * 360;//左前轮
-    ClassisSpeed[2] = sqrt(A * A + D * D) / (WHEEL_DIAMETER * PI) * 360;//左后轮
-    ClassisSpeed[3] = -sqrt(A * A + C * C) / (WHEEL_DIAMETER * PI) * 360;//右后轮
+    ClassisSpeed[0] = sqrt(B * B + D * D)  / (WHEEL_DIAMETER * PI) * 360;//右前轮1
+    ClassisSpeed[1] = -sqrt(B * B + C * C)/ (WHEEL_DIAMETER * PI) * 360;//左前轮2
+    ClassisSpeed[2] = sqrt(A * A + D * D) / (WHEEL_DIAMETER * PI) * 360;//左后轮3
+    ClassisSpeed[3] = -sqrt(A * A + C * C) / (WHEEL_DIAMETER * PI) * 360;//右后轮4
 
     float nowAngle[4] = {SFR.nowAngle - SFR.zeroAngle, SFL.nowAngle - SFL.zeroAngle,
                          SBL.nowAngle - SBL.zeroAngle, SBR.nowAngle - SBR.zeroAngle};
-
-    /*修正角度*/
-    for (int k = -3; k <= 3; k++) {
-        if (abs(RFRAngle + k * 180 - nowAngle[0]) <= 90) {
-            RFRAngle += (k * 180);
-            if (abs(k) % 2) {
-                ClassisSpeed[0] = -ClassisSpeed[0];
-            }
-            break;
-        }
-    }
-    for (int k = -3; k <= 3; k++) {
-        if (abs(RFLAngle + k * 180 - nowAngle[1]) <= 90) {
-            RFLAngle += (k * 180);
-            if (abs(k) % 2) {
-                ClassisSpeed[1] = -ClassisSpeed[1];
-            }
-            break;
-        }
-    }
-    for (int k = -3; k <= 3; k++) {
-        if (abs(RBLAngle + k * 180 - nowAngle[2]) <= 90) {
-            RBLAngle += (k * 180);
-            if (abs(k) % 2) {
-                ClassisSpeed[2] = -ClassisSpeed[2];
-            }
-            break;
-        }
-    }
-    for (int k = -3; k <= 3; k++) {
-        if (abs(RBRAngle + k * 180 - nowAngle[3]) <= 90) {
-            RBRAngle += (k * 180);
-            if (abs(k) % 2) {
-                ClassisSpeed[3] = -ClassisSpeed[3];
-            }
-            break;
-        }
-    }
 
     //设置底盘电机角度
     SFL.SetTargetAngle(RFLAngle);
