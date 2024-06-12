783049298aba6588b473d2dc685ef85ca4590bf2
bddwyx <bddwyx@sjtu.edu.cn>
2024-06-12 17:32:47 +0800

    refactor(Algorithms): 控制架构重构

---
+++ Algorithms/Control/ControlBase.h
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * Copyright (c) 2024.
+ * IWIN-FINS Lab, Shanghai Jiao Tong University, Shanghai, China.
+ * All rights reserved.
+ ******************************************************************************/
+
+#ifndef FINEMOTE_CONTROLBASE_H
+#define FINEMOTE_CONTROLBASE_H
+
+#include <utility>
+#include <vector>
+
+/**
+ * 控制器的输出应当不是一个立即数，而是根据指定地址存放数据，从而保证实际控制发生前控制量的生命周期
+ */
+
+class ControllerBase {
+public:
+    ControllerBase() = default;
+    explicit ControllerBase(float* _targetPtr) : targetPtr(_targetPtr) {}
+
+    virtual const float& Calc() = 0;
+
+    void Cascade(ControllerBase& next) {
+        next.targetPtr = &output;
+    }
+
+    const float& GetOutput() const {
+        return output;
+    }
+
+    void SetTarget(float* _targetPtr) {
+        this->targetPtr = _targetPtr;
+    }
+
+    virtual void SetFeedback(const std::vector<float*>& feedbackPtrs) {
+        this->feedbackPtr = feedbackPtrs[0];
+    }
+
+protected:
+    float* targetPtr = nullptr;
+    float* feedbackPtr = nullptr;
+    float output = 0;
+};
+
+template<typename T, size_t M, typename... Args, size_t... I>
+std::array<T, M> CreateControllersImpl(std::index_sequence<I...>, Args&&... args) {
+    return { (static_cast<void>(I), T{std::forward<Args>(args)...})... };
+}
+
+template<typename T, size_t M, typename... Args>
+auto CreateControllers(Args&&... args) {
+    static_assert(!std::is_same<T, ControllerBase>::value, "ControllerBase is not allowed");
+    static_assert(sizeof...(Args) <= 1, "Only one parameter is allowed");
+    return CreateControllersImpl<T, M>(std::make_index_sequence<M>{}, std::forward<Args>(args)...);
+}
+
+template<template<size_t> typename T, size_t M, typename... Args>
+auto CreateControllers(Args&&... args) {
+    constexpr size_t N = sizeof...(Args);
+    return CreateControllersImpl<T<N>, M>(std::make_index_sequence<M>{}, std::forward<Args>(args)...);
+}
+
+template <int K = 1>
+class Amplifier : public ControllerBase {
+    const float& Calc() final {
+        output = K * (*targetPtr);
+        return output;
+    }
+};
+
+#endif //FINEMOTE_CONTROLBASE_H
--- Algorithms/Control/PID.h
+++ Algorithms/Control/PID.h
@@ -7,34 +7,77 @@
 #ifndef FINEMOTE_PID_H
 #define FINEMOTE_PID_H
 
-# include "Interface.h"
+#include <vector>
 
-typedef struct PID_Regulator {
-    float ref;
-    float fdb;
-    float err[4];
-    float errSum;
+#include "ControlBase.h"
+
+#define INRANGE(NUM, MIN, MAX) \
+    NUM = ((NUM > (MIN) ? NUM : (MIN)) \
+          < (MAX) ? NUM : (MAX))
+
+typedef struct PID_Param_t {
     float kp;
     float ki;
     float kd;
-    float componentKp;
-    float componentKi;
-    float componentKd;
-    float componentKpMax;
-    float componentKiMax;
-    float componentKdMax;
-    float output;
+    float iMax;
     float outputMax;
-} PID_Regulator_t;
+} PID_Param_t;
 
-class PID{
+class PID : public ControllerBase {
 public:
+    constexpr explicit PID(const PID_Param_t& params) : params(params) {}
 
-    PID_Regulator_t PIDInfo{};
-    void Reset(PID_Regulator_t * pidRegulator);
-    void Reset();
-    float PIDCalc(float target,float feedback);
-    float PIDCalc(float target,float feedback,float max);
+    const float& Calc() override{
+        float error = *targetPtr - *feedbackPtr;
+        totalError += error;
+        INRANGE(totalError, -1 * params.iMax, params.iMax);
+        output = params.kp * error + params.ki * totalError + params.kd * (error - lastError);
+        lastError = error;
+        return INRANGE(output, -1 * params.outputMax, params.outputMax);
+    }
+
+private:
+    const PID_Param_t params;
+    float totalError = 0, lastError = 0;
+};
+
+template<int K>
+class CascadePID : public PID {
+public:
+    template<typename... Params>
+    constexpr explicit CascadePID(const PID_Param_t& first, Params... params) : PID(first), nodes{PID(params)...} {
+        static_assert(K > 1, "CascadePID should have at least 2 layers");
+
+        Cascade(nodes[0]);
+        for (int i = 0; i < K - 2; ++i) {
+            nodes[i].Cascade(nodes[i + 1]);
+        }
+    }
+
+    const float& Calc() final{
+        PID::Calc();
+        for(auto& node : nodes) {
+            node.Calc();
+        }
+
+        output = nodes.rbegin()->GetOutput();
+        return output;
+    }
+
+    void SetFeedback(const std::vector<float*>& feedbackPtrs) final {
+        //static_assert(K == feedbackPtrs.size(), "Feedback number should be equal to K");
+
+        auto iter = feedbackPtrs.begin();
+        PID::SetFeedback({*iter});
+        iter++;
+        for(auto& node : nodes) {
+            node.SetFeedback({*iter});
+            iter++;
+        }
+    }
+
+private:
+    std::array<PID, K - 1> nodes;
 };
 
 #endif //FINEMOTE_PID_H
