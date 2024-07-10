bf351de728ae00ba2cf4efeabd594b1fbf86b54c
bddwyx <bddwyx@sjtu.edu.cn>
2024-07-10 15:09:41 +0800

    refactor(BSP): 修改HAL初始化逻辑

--- BSP/MC_Board/MC_Board.h
+++ BSP/MC_Board/MC_Board.h
@@ -23,11 +23,11 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-extern void SystemClock_Config();
-extern void SystemClock_PreConfig();
-extern void SystemClock_PostConfig();
+
+extern int main();
+
 #ifdef __cplusplus
-};
+}
 #endif
 
 class HALInit {
@@ -35,35 +35,7 @@ class HALInit {
  *  @attention 此函数需要与cube生成的保持一致
  */
     HALInit() {
-
-        HAL_Init();
-        SystemClock_PreConfig();
-        SystemClock_Config();
-        SystemClock_PostConfig();
-
-        //* USER CODE BEGIN SysInit *//
-
-        //* USER CODE END SysInit *//
-
-        //* Initialize all configured peripherals *//
-        MX_GPIO_Init();
-        MX_DMA_Init();
-        MX_CAN1_Init();
-        MX_CAN2_Init();
-        MX_SPI1_Init();
-        MX_UART5_Init();
-        MX_USART1_UART_Init();
-        MX_ADC1_Init();
-        MX_I2C1_Init();
-        MX_TIM8_Init();
-        MX_I2C3_Init();
-        MX_USART2_UART_Init();
-        MX_TIM3_Init();
-        MX_TIM2_Init();
-        MX_TIM7_Init();
-//        MX_IWDG_Init();
-        MX_USART3_UART_Init();
-        MX_SPI2_Init();
+        main();
     };
 
 public:
--- BSP/Robomaster_C/Robomaster_C.h
+++ BSP/Robomaster_C/Robomaster_C.h
@@ -21,46 +21,20 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-extern void SystemClock_Config();
+
+extern int main();
+
 #ifdef __cplusplus
-};
+}
 #endif
 
 class HALInit{
 /**
  *  @attention 此函数需要与cube生成的保持一致
  */
-    HALInit(){
-        HAL_Init();
-        SystemClock_Config();
-
-        /* USER CODE BEGIN SysInit */
-
-        /* USER CODE END SysInit */
-
-        /* Initialize all configured peripherals */
-        MX_GPIO_Init();
-        MX_DMA_Init();
-        MX_ADC1_Init();
-        MX_ADC3_Init();
-        MX_CAN1_Init();
-        MX_CAN2_Init();
-        MX_SPI1_Init();
-        MX_TIM4_Init();
-        MX_TIM5_Init();
-        MX_USART3_UART_Init();
-        MX_TIM8_Init();
-        MX_I2C3_Init();
-        MX_TIM1_Init();
-        MX_TIM3_Init();
-        MX_TIM10_Init();
-        MX_USART1_UART_Init();
-        MX_USART6_UART_Init();
-        MX_TIM6_Init();
-       // MX_IWDG_Init();
-        MX_TIM7_Init();
-        MX_I2C2_Init();
-    };
+    HALInit() {
+        main();
+    }
 
 public:
     HALInit(const HALInit &) = delete;
