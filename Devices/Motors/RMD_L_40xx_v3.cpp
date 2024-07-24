3073cf4925f07640a92775eca07198ed96bb1315
bddwyx <bddwyx@sjtu.edu.cn>
2024-07-24 21:51:41 +0800

    feat(Devices): 增加第三代脉塔CAN协议驱动支持

---
+++ Devices/Motors/RMD_L_40xx_v3.cpp
@@ -0,0 +1,7 @@
+/*******************************************************************************
+ * Copyright (c) 2024.
+ * IWIN-FINS Lab, Shanghai Jiao Tong University, Shanghai, China.
+ * All rights reserved.
+ ******************************************************************************/
+
+#include "RMD_L_40xx_v3.h"
---
+++ Devices/Motors/RMD_L_40xx_v3.h
@@ -0,0 +1,94 @@
+/*******************************************************************************
+ * Copyright (c) 2024.
+ * IWIN-FINS Lab, Shanghai Jiao Tong University, Shanghai, China.
+ * All rights reserved.
+ ******************************************************************************/
+
+#ifndef FINEMOTE_RMD_L_40xx_V3_H
+#define FINEMOTE_RMD_L_40xx_V3_H
+
+#include "ProjectConfig.h"
+
+#ifdef MOTOR_COMPONENTS
+
+#include "DeviceBase.h"
+#include "Bus/CAN_Base.h"
+#include "Motors/MotorBase.h"
+
+/**
+ * Todo: Reduction ratio
+ */
+template<int busID>
+class RMD_L_40xx_v3 : public MotorBase {
+public:
+
+    template<typename T>
+    RMD_L_40xx_v3(const Motor_Param_t&& params, T& _controller, uint32_t addr) : MotorBase(std::forward<const Motor_Param_t>(params)), canAgent(addr) {
+        SetDivisionFactor(5);
+        ResetController(_controller);
+    }
+
+    void Handle() final{
+        Update();
+        controller->Calc();
+        MessageGenerate();
+    };
+
+    CAN_Agent<busID> canAgent;
+
+private:
+    void SetFeedback() final{
+        switch (params.targetType) {
+            case Motor_Ctrl_Type_e::Position:
+                controller->SetFeedback({&state.position, &state.speed});
+                break;
+            case Motor_Ctrl_Type_e::Speed:
+                controller->SetFeedback({&state.speed});
+                break;
+        }
+    }
+
+    void MessageGenerate() {
+        switch (params.ctrlType) {
+            case Motor_Ctrl_Type_e::Torque: {
+                volatile int16_t txTorque = controller->GetOutput();
+                INRANGE(txTorque,-2000,2000);
+
+                canAgent[0] = 0xA1;
+                canAgent[1] = 0x00;
+                canAgent[2] = 0x00;
+                canAgent[3] = 0x00;
+                canAgent[4] = txTorque;
+                canAgent[5] = txTorque >> 8;
+                canAgent[6] = 0x00;
+                canAgent[7] = 0x00;
+                break;
+            }
+            case Motor_Ctrl_Type_e::Position: {
+                constexpr uint16_t txSpeed = 0x800;
+                int32_t txAngle = 100 * controller->GetOutput() * -1; //统一正方向
+
+                canAgent[0] = 0xA4;
+                canAgent[1] = 0x00;
+                canAgent[2] = txSpeed;
+                canAgent[3] = txSpeed >> 8;
+                canAgent[4] = txAngle;
+                canAgent[5] = txAngle >> 8;
+                canAgent[6] = txAngle >> 16;
+                canAgent[7] = txAngle >> 24;
+                break;
+            }
+        }
+        canAgent.Send(canAgent.addr - 0x100);
+    }
+
+    void Update() {
+        state.position = (canAgent.rxbuf[6] | (canAgent.rxbuf[7] << 8u)) * 360.0f / 16384.0f;
+        state.speed = (int16_t)(canAgent.rxbuf[4] | (canAgent.rxbuf[5] << 8u));
+        state.torque = (int16_t)(canAgent.rxbuf[2] | (canAgent.rxbuf[3] << 8u));
+        state.temperature = (int8_t)(canAgent.rxbuf[1]);
+    }
+};
+
+#endif MOTOR_COMPONENTS
+#endif FINEMOTE_RMD_L_40xx_V3_H
