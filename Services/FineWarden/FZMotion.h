f04a6bbed9c09587b05384acc5b689d8d73dc9bd
freude9 <75164063+freude9@users.noreply.github.com>
2024-08-14 22:11:27 +0800

    feat(Services): 补全FZMotion数据解析

--- Services/FineWarden/FZMotion.h
+++ Services/FineWarden/FZMotion.h
@@ -31,14 +31,62 @@ typedef struct{
     float qua_W;
 } __packed MQTT_rigid_msg_t;
 
+typedef struct{
+    float pos_X;
+    float pos_Y;
+    float pos_Z;
+    float vel_X;
+    float vel_Y;
+    float vel_Z;
+    float eul_X;
+    float eul_Y;
+    float eul_Z;
+} __packed mqtt_brief_t;
+
+#define RIGID_NUMBER 3
+typedef struct{
+    uint32_t seq_pack;
+    double cam_time;
+    bool rig_0_valid:1;
+    bool rig_1_valid:1;
+    bool rig_2_valid:1;
+    bool rig_3_valid:1;
+    bool rig_4_valid:1;
+    bool rig_5_valid:1;
+    bool rig_6_valid:1;
+    bool rig_7_valid:1;
+    mqtt_brief_t rig0_info;
+    mqtt_brief_t rig1_info;
+    mqtt_brief_t rig2_info;
+#if RIGID_NUMBER > 3
+    mqtt_brief_t rig3_info;
+#endif
+#if RIGID_NUMBER > 4
+    mqtt_brief_t rig4_info;
+#endif
+#if RIGID_NUMBER > 5
+    mqtt_brief_t rig5_info;
+#endif
+#if RIGID_NUMBER > 6
+    mqtt_brief_t rig6_info;
+#endif
+#if RIGID_NUMBER > 7
+    mqtt_brief_t rig7_info;
+#endif
+
+} __packed mqtt_brief_composed_t;
+
 class FZMotion {
 public:
     void Decode(uint8_t* data, uint16_t size){
-        if (size >= sizeof(rxBuffer)) memcpy(&rxBuffer, data, sizeof(MQTT_rigid_msg_t));
+        if (size >= sizeof(rxBuffer)) memcpy(&rxBuffer, data, sizeof(rxBuffer));
     };
+    mqtt_brief_composed_t GetData(){
+        return rxBuffer;
+    }
 
 private:
-    MQTT_rigid_msg_t rxBuffer;
+    mqtt_brief_composed_t rxBuffer;
 };
 
 #endif //FINEMOTE_FZMOTION_H
