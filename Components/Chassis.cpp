754fd5e416d954709c10d7e2875bfee345f75843
ganju2390 <143862324+ganju2390@users.noreply.github.com>
2024-09-12 12:59:35 +0800

    feat(Components): 加入里程计和速度规划功能

--- Components/Chassis.cpp
+++ Components/Chassis.cpp
@@ -5,75 +5,198 @@
  ******************************************************************************/
 
 #include "Chassis.h"
-#include <cmath>
 
 void Chassis::ChassisSetVelocity(float _fbV, float _lrV, float _rtV) {
-    ChassisStopFlag = false;
-    FBVelocity = _fbV;
-    LRVelocity = _lrV;
-    RTVelocity = _rtV;
+	ChassisStopFlag = false;
+	FBVelocity = _fbV;
+	LRVelocity = _lrV;
+	RTVelocity = _rtV;
 }
 
 void Chassis::ChassisStop() {
-    ChassisStopFlag = true;
+	ChassisStopFlag = true;
 
-    //Classis_Motor.Stop();
-    SFL.Stop();
-    SFR.Stop();
-    SBL.Stop();
-    SBR.Stop();
-    CFR.Stop();
-    CFL.Stop();
-    CBL.Stop();
-    CBR.Stop();
+	SFL.Stop();
+	SFR.Stop();
+	SBL.Stop();
+	SBR.Stop();
+	CFR.Stop();
+	CFL.Stop();
+	CBL.Stop();
+	CBR.Stop();
 }
 
-void Chassis::WheelsSpeedCalc(float fbVelocity, float lrVelocity, float rtVelocity) {
-    float ClassisSpeed[4];
-    float RFLAngle, RFRAngle, RBLAngle, RBRAngle;
-    float vx, vy, w;
-    vx = lrVelocity;
-    vy = fbVelocity;
-    w = rtVelocity;
-    float A, B, C, D;
-    A = vx + w * LENGTH / 2;
-    B = vx - w * LENGTH / 2;
-    C = vy + w * WIDTH / 2;
-    D = vy - w * WIDTH / 2;
+void Chassis::Odometry() {
+	struct WheelSet {
+		WheelSet(const float _angle, const float _vel) {
+			angle = _angle / 180.f * PI; //弧度制
+			float tmp[2];
+			tmp[1] = _vel / 360.f * PI * WHEEL_DIAMETER * cosf(angle); //单位m/s
+			tmp[0] = _vel / 360.f * PI * WHEEL_DIAMETER * sinf(angle);
+			vel = tmp;
+		}
 
-    //计算四个轮子角度，单位：度
-    RFRAngle = atan2(A, D) * 180 / PI;
-    RFLAngle = atan2(A, C) * 180 / PI;
-    RBLAngle = atan2(B, C) * 180 / PI;
-    RBRAngle = atan2(B, D) * 180 / PI;
+		float angle{0};
+		Matrixf<2, 1> vel;
+	};
 
-    //计算四个轮子线速度，单位：度/s
-    ClassisSpeed[0] = -sqrt(A * A + D * D)  / (WHEEL_DIAMETER * PI) * 360;//右前轮1
-    ClassisSpeed[1] = sqrt(A * A + C * C)/ (WHEEL_DIAMETER * PI) * 360;//左前轮2
-    ClassisSpeed[2] = sqrt(B * B + C * C) / (WHEEL_DIAMETER * PI) * 360;//左后轮3
-    ClassisSpeed[3] = -sqrt(B * B + D * D) / (WHEEL_DIAMETER * PI) * 360;//右后轮4
+	WheelSet FR(SFR.GetState().position, -CFR.GetState().speed); //右侧轮毂电机反转，取负
+	WheelSet FL(SFL.GetState().position, CFL.GetState().speed);
+	WheelSet BL(SBL.GetState().position, CBL.GetState().speed);
+	WheelSet BR(SBR.GetState().position, -CBR.GetState().speed);
 
-/*    float nowAngle[4] = {SFR.state.position - SFR.zeroAngle, SFL.state.position - SFL.zeroAngle,
-                         SBL.state.position - SBL.zeroAngle, SBR.state.position - SBR.zeroAngle};*/
+	chassisVel = invA * (H1.trans() * FR.vel + H2.trans() * FL.vel + H3.trans() *
+		BL.vel + H4.trans() * BR.vel);
+	float WCSVeldata[3 * 1] = {
+		chassisVel[0][0] * cosf(chassisPos[0][2]) + chassisVel[1][0] * sinf(chassisPos[0][2]),
+		-chassisVel[0][0] * sinf(chassisPos[0][2]) + chassisVel[1][0] * cosf(chassisPos[0][2]),
+		chassisVel[0][2]
+	};
+	WCSVelocity = Matrixf<3, 1>(WCSVeldata);
+	chassisPos += WCSVelocity * 0.001;
 
-    //设置底盘电机角度
-    SFR.SetTargetAngle(RFRAngle);
-    SFL.SetTargetAngle(RFLAngle);
-    SBL.SetTargetAngle(RBLAngle);
-    SBR.SetTargetAngle(RBRAngle);
-
-    //设置底盘电机转速
-    CFR.SetTargetSpeed(ClassisSpeed[0]);
-    CFL.SetTargetSpeed(ClassisSpeed[1]);
-    CBL.SetTargetSpeed(ClassisSpeed[2]);
-    CBR.SetTargetSpeed(ClassisSpeed[3]);
+	FRX=FR.vel[0][0],FRY=FR.vel[1][0];
+	FLX=FL.vel[0][0],FLY=FL.vel[1][0];
+	BLX=BL.vel[0][0],BLY=BL.vel[1][0];
+	BRX=BR.vel[0][0],BRY=BR.vel[1][0];
 
 }
+
+void Chassis::ICFOdometry() {
+	struct WheelSet {
+		WheelSet(const float _angle, const float _vel) {
+			angle = _angle / 180.f * PI; //弧度制
+			float tmp[2];
+			tmp[1] = _vel / 360.f * PI * WHEEL_DIAMETER * cosf(angle); //单位m/s
+			tmp[0] = _vel / 360.f * PI * WHEEL_DIAMETER * sinf(angle);
+			vel = tmp;
+		}
+		float angle{0};
+		Matrixf<2, 1> vel;
+	};
+
+	WheelSet FR(SFR.GetState().position, -CFR.GetState().speed); //右侧轮毂电机反转，取负
+	WheelSet FL(SFL.GetState().position, CFL.GetState().speed);
+	WheelSet BL(SBL.GetState().position, CBL.GetState().speed);
+	WheelSet BR(SBR.GetState().position, -CBR.GetState().speed);
+
+	FRX=FR.vel[0][0],FRY=FR.vel[1][0];
+	FLX=FL.vel[0][0],FLY=FL.vel[1][0];
+	BLX=BL.vel[0][0],BLY=BL.vel[1][0];
+	BRX=BR.vel[0][0],BRY=BR.vel[1][0];
+
+	Matrixf<3,3> V1=0.25*J1+H1.trans()*B*H1;
+	Matrixf<3,1> v1=0.25*J1*x1+H1.trans()*B*FR.vel;
+	Matrixf<3,3> V2=0.25*J2+H2.trans()*B*H2;
+	Matrixf<3,1> v2=0.25*J2*x2+H2.trans()*B*FL.vel;
+	Matrixf<3,3> V3=0.25*J3+H3.trans()*B*H3;
+	Matrixf<3,1> v3=0.25*J3*x3+H3.trans()*B*BL.vel;
+	Matrixf<3,3> V4=0.25*J4+H4.trans()*B*H4;
+	Matrixf<3,1> v4=0.25*J4*x4+H4.trans()*B*BR.vel;
+
+	Matrixf<3,3> V1_t=(1-3*epi)*V1+epi*(V2+V3+V4);
+	Matrixf<3,1> v1_t=(1-3*epi)*v1+epi*(v2+v3+v4);
+	Matrixf<3,3> V2_t=(1-3*epi)*V2+epi*(V1+V3+V4);
+	Matrixf<3,1> v2_t=(1-3*epi)*v2+epi*(v1+v3+v4);
+	Matrixf<3,3> V3_t=(1-3*epi)*V3+epi*(V2+V1+V4);
+	Matrixf<3,1> v3_t=(1-3*epi)*v3+epi*(v2+v1+v4);
+	Matrixf<3,3> V4_t=(1-3*epi)*V4+epi*(V2+V3+V1);
+	Matrixf<3,1> v4_t=(1-3*epi)*v4+epi*(v2+v3+v1);
+
+	V1=V1_t,v1=v1_t;
+	V2=V2_t,v2=v2_t;
+	V3=V3_t,v3=v3_t;
+	V4=V4_t,v4=v4_t;
+
+	x1=matrixf::inv(V1)*v1;
+	J1=4*V1;
+	x2=matrixf::inv(V2)*v2;
+	J2=4*V2;
+	x3=matrixf::inv(V3)*v3;
+	J3=4*V3;
+	x4=matrixf::inv(V4)*v4;
+	J4=4*V4;
+
+	J1=matrixf::inv(matrixf::inv(J1)+Q);
+	J2=matrixf::inv(matrixf::inv(J2)+Q);
+	J3=matrixf::inv(matrixf::inv(J3)+Q);
+	J4=matrixf::inv(matrixf::inv(J4)+Q);
+
+	float WCSVeldata[3 * 1] = {
+		x1[0][0] * cosf(chassisPos[0][2]) + x1[1][0] * sinf(chassisPos[0][2]),
+		-x1[0][0] * sinf(chassisPos[0][2]) + x1[1][0] * cosf(chassisPos[0][2]),
+		x1[0][2]
+	};
+	WCSVelocity = Matrixf<3, 1>(WCSVeldata);
+	chassisPos += WCSVelocity * 0.001;
+}
+
+
+void Chassis::WheelsSpeedCalc(float fbVelocity, float lrVelocity,float rtVelocity) {
+	float ClassisSpeed[4];
+	float RFLAngle, RFRAngle, RBLAngle, RBRAngle;
+	float vx, vy, w;
+
+	vx = lrVelocity;
+	vy = fbVelocity;
+	w = rtVelocity;
+	float A, B, C, D;
+	A = vx + w * LENGTH / 2;
+	B = vx - w * LENGTH / 2;
+	C = vy + w * WIDTH / 2;
+	D = vy - w * WIDTH / 2;
+
+	//计算四个轮子角度，单位：度
+	RFRAngle = atan2f(A, D) * 180 / PI;
+	RFLAngle = atan2f(A, C) * 180 / PI;
+	RBLAngle = atan2f(B, C) * 180 / PI;
+	RBRAngle = atan2f(B, D) * 180 / PI;
+
+	//计算四个轮子线速度，单位：度/s
+	ClassisSpeed[0] = -sqrtf(A * A + D * D) / (WHEEL_DIAMETER * PI) * 360; //右前轮1
+	ClassisSpeed[1] = sqrtf(A * A + C * C) / (WHEEL_DIAMETER * PI) * 360; //左前轮2
+	ClassisSpeed[2] = sqrtf(B * B + C * C) / (WHEEL_DIAMETER * PI) * 360; //左后轮3
+	ClassisSpeed[3] = -sqrtf(B * B + D * D) / (WHEEL_DIAMETER * PI) * 360; //右后轮4
+
+	//设置底盘电机角度
+	SFR.SetTargetAngle(RFRAngle);
+	SFL.SetTargetAngle(RFLAngle);
+	SBL.SetTargetAngle(RBLAngle);
+	SBR.SetTargetAngle(RBRAngle);
+
+	//设置底盘电机转速
+	CFR.SetTargetSpeed(ClassisSpeed[0]);
+	CFL.SetTargetSpeed(ClassisSpeed[1]);
+	CBL.SetTargetSpeed(ClassisSpeed[2]);
+	CBR.SetTargetSpeed(ClassisSpeed[3]);
+}
+
 
 void Chassis::Handle() {
-    WheelsSpeedCalc(FBVelocity, LRVelocity, RTVelocity);
+	WheelsSpeedCalc(FBVelocity, LRVelocity, RTVelocity);
+	// Odometry();
+	ICFOdometry();
 }
 
+
 ChassisBuilder Chassis::Build() {
-    return {};
+	return {};
 }
+
+void Chassis::ResetOdometry(float _x=0,float _y=0,float _angle=0) {
+	// chassisPos = matrixf::zeros<3, 1>();
+	chassisPos[0][0] = _x;
+	x=_x;
+	chassisPos[1][0] = _y;
+	y=_y;
+	chassisPos[2][0] = _angle;
+	yaw=_angle;
+}
+
+
+
+
+
+
+
+
--- Components/Chassis.h
+++ Components/Chassis.h
@@ -8,16 +8,19 @@
 #define FINEMOTE_CHASSIS_H
 
 #include "ProjectConfig.h"
-
+#include "Matrix/matrix.h"
 #include "DeviceBase.h"
 #include "MotorBase.h"
+#include <queue>
+#include <cmath>
 
-#define LENGTH  0.24f //车身长
+#define LENGTH  0.2406f //车身长
 #define WIDTH  0.24f //车身宽
-#define WHEEL_DIAMETER 0.0528f//4010直径 m
+#define WHEEL_DIAMETER 0.0525f//4010直径 m
 #define PI 3.1415926f
 
 class ChassisBuilder;
+class VelocityProfile;
 
 class Chassis : public DeviceBase {
     friend class ChassisBuilder;
@@ -37,15 +40,64 @@ class Chassis : public DeviceBase {
     SFL(*SFLPtr),
     SFR(*SFRPtr),
     SBL(*SBLPtr),
-    SBR(*SBRPtr){}
+    SBR(*SBRPtr){
+
+    float H1Data[2*3] = {1,0,LENGTH/2,
+                         0,1,-WIDTH/2};
+    H1 = Matrixf<2,3>(H1Data);
+    float H2Data[2*3] = {1,0,LENGTH/2,
+                         0,1,WIDTH/2};
+    H2 = Matrixf<2,3>(H2Data);
+    float H3Data[2*3] = {1,0,-LENGTH/2,
+                         0,1,WIDTH/2};
+    H3 = Matrixf<2,3>(H3Data);
+    float H4Data[2*3] = {1,0,-LENGTH/2,
+                         0,1,-WIDTH/2};
+    H4 = Matrixf<2,3>(H4Data);
+    float AData[3*3] = {4,0,0,
+                        0,4,0,
+                        0,0,4*(LENGTH*LENGTH/4+WIDTH*WIDTH/4)};
+    Matrixf<3,3> A(AData);
+    invA = matrixf::inv(A);
+
+    float BData[2*2] = {0.05,0,
+                        0,0.05};
+    B = Matrixf<2,2>(BData);
+    float QData[3*3] = {0.1,0,0,
+                        0,0.1,0,
+                        0,0,0.1};
+    Q=Matrixf<3,3>(QData);
+    }
 
 public:
     void ChassisSetVelocity(float _fbV, float _lrV, float _rtV);
     void ChassisStop();
     void WheelsSpeedCalc(float fbVelocity, float lrVelocity, float rtVelocity);
     void Handle() override;
+    void Odometry();
+    void ICFOdometry();
+    void ResetOdometry(float _x,float _y,float _angle);
     bool ChassisStopFlag = true;
     float FBVelocity{}, LRVelocity{}, RTVelocity{};
+    float epi = 0.2;
+    float x{0},y{0},yaw{0};
+    float FRX,FRY,FLX,FLY,BLX,BLY,BRX,BRY;
+
+    Matrixf<3,3> invA;
+    Matrixf<2,3> H1,H2,H3,H4;
+    Matrixf<2,2> B;
+    Matrixf<3,3> Q;
+    Matrixf<3,3> J1=matrixf::eye<3,3>();
+    Matrixf<3,3> J2=matrixf::eye<3,3>();
+    Matrixf<3,3> J3=matrixf::eye<3,3>();
+    Matrixf<3,3> J4=matrixf::eye<3,3>();
+    Matrixf<3,1> x1=matrixf::zeros<3,1>();
+    Matrixf<3,1> x2=matrixf::zeros<3,1>();
+    Matrixf<3,1> x3=matrixf::zeros<3,1>();
+    Matrixf<3,1> x4=matrixf::zeros<3,1>();
+    Matrixf<3,1> chassisVel = matrixf::zeros<3,1>();
+    Matrixf<3,1> chassisPos = matrixf::zeros<3,1>();
+    Matrixf<3,1> WCSVelocity = matrixf::zeros<3,1>();
 
     MotorBase &CFL,&CFR,&CBL,&CBR;
     MotorBase &SFL,&SFR,&SBL,&SBR;
@@ -83,5 +135,139 @@ public:
 };
 
 
+class VelocityProfile : public DeviceBase {
+public:
+    VelocityProfile(float _targetTime, float _initX, float _endX, float _initVel,
+                    float _endVel): targetTime(_targetTime), targetPos(_endX) {
+        float TData[4 * 4]
+        {
+            0, 0, 0, 1,
+            _targetTime * _targetTime * _targetTime, _targetTime * _targetTime, _targetTime, 1,
+            0, 0, 1, 0,
+            3 * _targetTime * _targetTime, 2 * _targetTime, 1, 0
+        };
+        T = Matrixf<4, 4>(TData);
+
+        float resultData[4 * 1]
+        {
+            _initX, _endX, _initVel, _endVel
+        };
+        result = Matrixf<4, 1>(resultData);
+
+        constant = matrixf::inv(T) * result;
+        initTick = HAL_GetTick();
+    }
+
+    void CalcuateVelAndPos() {
+        if (currentTime > targetTime) {
+            output[0][0] = targetPos;
+            output[1][0] = 0.0f;
+            return;
+        }
+        float inputData[2 * 4]
+        {
+            currentTime * currentTime * currentTime, currentTime * currentTime, currentTime, 1,
+            3 * currentTime * currentTime, 2 * currentTime, 1, 0
+        };
+        input = Matrixf<2, 4>(inputData);
+        output = input * constant;
+    }
+
+    Matrixf<2, 1>& GetOutput() {
+        return output;
+    }
+
+    float GetCurrentTime() const {
+        return currentTime;
+    }
+
+    void Handle() override {
+        currentTick = HAL_GetTick();
+        currentTime = (float)(currentTick - initTick) * 0.001f;
+        CalcuateVelAndPos();
+    }
+
+private:
+    Matrixf<4, 1> constant;
+    Matrixf<4, 4> T;
+    Matrixf<4, 1> result;
+    Matrixf<2, 1> output = matrixf::zeros<2, 1>(); //分别为位置和速度
+    Matrixf<2, 4> input = matrixf::zeros<2, 4>();
+    float targetTime{10.0f};
+    float currentTime{0.0f};
+    float targetPos{0.0f};
+    uint32_t initTick, currentTick;
+};
+
+
+class RoutePlanning {
+    struct Target {
+        float x, xVel, y, yVel, angle, angleVel, time;
+
+        Target(const float _x, const float _xVel, const float _y, const float _yVel, const float _angle,
+               const float _angleVel, const float _time): x(_x), y(_y), angle(_angle), time(_time), xVel(_xVel),
+                                                          yVel(_yVel), angleVel(_angleVel) {
+        }
+    };
+
+public:
+    float RTVel{0}, FBVel{0}, LRVel{0};
+    bool isFinished = false;
+
+    RoutePlanning(const float _Kp): currentX(0), currentY(0), currentAngle(0), Kp(_Kp), target(0, 0, 0, 0, 0, 0, 0) {
+    }
+
+    void AddTarget(float x, float xVel, float y, float yVel, float angle, float angleVel, float time) {
+        targetList.emplace(x, xVel, y, yVel, angle, angleVel, time);
+    }
+
+    void Update(const float x, const float xVel, const float y, const float yVel, const float angle,
+                const float angleVel) {
+        currentX = x, currentY = y, currentAngle = angle;
+        currentXVel = xVel, currentYVel = yVel, currentAngleVel = angleVel;
+    }
+
+    void CalcSpeed() {
+        if (isArrived) {
+            if (targetList.empty()) {
+                isFinished = true;
+                FBVel = 0, LRVel = 0, RTVel = 0;
+                return;
+            }
+            delete XVelProfilePtr;
+            delete YVelProfilePtr;
+            delete RTVelProfilePtr;
+
+            target = targetList.front();
+            targetList.pop();
+
+            XVelProfilePtr = new VelocityProfile(target.time, currentX, target.x, currentXVel, target.xVel);
+            YVelProfilePtr = new VelocityProfile(target.time, currentY, target.y, currentYVel, target.yVel);
+            RTVelProfilePtr = new VelocityProfile(target.time, currentAngle, target.angle, currentAngleVel,target.angleVel);
+
+            isArrived = false;
+        }
+        float wcs_vel_x = XVelProfilePtr->GetOutput()[1][0] + (XVelProfilePtr->GetOutput()[0][0] - currentX) * Kp;
+        float wcs_vel_y = YVelProfilePtr->GetOutput()[1][0] + (YVelProfilePtr->GetOutput()[0][0] - currentY) * Kp;
+        float wcs_vel_rt = RTVelProfilePtr->GetOutput()[1][0] + (RTVelProfilePtr->GetOutput()[0][0] - currentAngle) *
+            Kp;
+
+        FBVel = cosf(currentAngle) * wcs_vel_y + sinf(currentAngle) * wcs_vel_x;
+        LRVel = -sinf(currentAngle) * wcs_vel_y + cosf(currentAngle) * wcs_vel_x;
+        RTVel = wcs_vel_rt;
+
+        isArrived = fabsf(target.angle - currentAngle) < 0.03 && fabsf(target.x - currentX) < 0.03 && fabsf(target.y - currentY) < 0.03;
+    }
+
+private:
+    std::queue<Target> targetList;
+    float currentX, currentY, currentAngle, currentXVel, currentYVel, currentAngleVel;
+    float Kp;
+    Target target;
+    bool isArrived = true;
+    VelocityProfile* XVelProfilePtr = nullptr;
+    VelocityProfile* YVelProfilePtr = nullptr;
+    VelocityProfile* RTVelProfilePtr = nullptr;
+};
 
 #endif //FINEMOTE_CHASSIS_H
