0544155cbddf825711b293a3ee8b8ca5e33ea0cd
bddwyx <bddwyx@sjtu.edu.cn>
2025-01-02 15:46:14 +0800

    refactor(Interface): 精简Interface
    只保留LED，蜂鸣器和底盘遥控器控制

--- Interface/Interface.cpp
+++ Interface/Interface.cpp
@@ -6,23 +6,20 @@
 
 #include "ProjectConfig.h"
 
-/*****  示例1 *****/
-#include "Emm28.h"
 #include "LED.h"
 #include "BeepMusic.h"
 #include "Chassis.h"
-#include "RMD_L_40xx_v3.h"
 #include "Motor4010.h"
 #include "Motor4315.h"
-#include "FZMotion.h"
+#include "RMD_L_40xx_v3.h"
 #include "RadioMaster_Zorro.h"
-#include "RadioMaster_Pocket.h"
 
+/*****  示例1 *****/
 /**
  * @brief LED闪烁
  */
 void Task1() {
-    static int cnt = 0;
+    static uint16_t cnt = 0;
     cnt++;
     if(cnt > 1000) {
         cnt = 0;
@@ -34,7 +31,6 @@ void Task1() {
 /**
  * @brief 音乐播放与切换
  */
-
 void Task2() {
     if(GPIO_PIN_RESET == HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_15)) {
         static int index = 1;
@@ -45,6 +41,9 @@ void Task2() {
 }
 
 /*****  示例3 *****/
+/**
+ * @brief 底盘根据遥控器数据运动
+ */
 
 constexpr PID_Param_t speedPID = {0.23f, 0.008f, 0.3f, 2000, 2000};
 
@@ -65,24 +64,24 @@ Motor4315<1> SFLMotor(DIRECT_POSITION, swerveControllers[2], 0x02);
 Motor4315<1> SFRMotor(DIRECT_POSITION, swerveControllers[3], 0x01);
 
 
-// constexpr PID_Param_t speedPID = {0.1f, 0.003f, 0.1f, 2000, 2000};
-//
-// auto wheelControllers = CreateControllers<PID, 4>(speedPID);
-// auto swerveControllers = CreateControllers<Amplifier<1>, 4>();
-//
-// #define TORQUE_2_SPEED {Motor_Ctrl_Type_e::Torque, Motor_Ctrl_Type_e::Speed}
-// #define TORQUE_2_POSITION {Motor_Ctrl_Type_e::Torque, Motor_Ctrl_Type_e::Position}
-// #define DIRECT_POSITION {Motor_Ctrl_Type_e::Position, Motor_Ctrl_Type_e::Position}
-//
-// RMD_L_40xx_v3<1> CFRMotor(TORQUE_2_SPEED, wheelControllers[0], 0x242);
-// RMD_L_40xx_v3<1> CFLMotor(TORQUE_2_SPEED, wheelControllers[1], 0x244);
-// RMD_L_40xx_v3<1> CBLMotor(TORQUE_2_SPEED, wheelControllers[2], 0x246);
-// RMD_L_40xx_v3<1> CBRMotor(TORQUE_2_SPEED, wheelControllers[3], 0x248);
-//
-// RMD_L_40xx_v3<1> SFRMotor(DIRECT_POSITION, swerveControllers[0], 0x241);
-// RMD_L_40xx_v3<1> SFLMotor(DIRECT_POSITION, swerveControllers[1], 0x243);
-// RMD_L_40xx_v3<1> SBLMotor(DIRECT_POSITION, swerveControllers[2], 0x245);
-// RMD_L_40xx_v3<1> SBRMotor(DIRECT_POSITION, swerveControllers[3], 0x247);
+/*constexpr PID_Param_t speedPID = {0.1f, 0.003f, 0.1f, 2000, 2000};
+
+auto wheelControllers = CreateControllers<PID, 4>(speedPID);
+auto swerveControllers = CreateControllers<Amplifier<1>, 4>();
+
+#define TORQUE_2_SPEED {Motor_Ctrl_Type_e::Torque, Motor_Ctrl_Type_e::Speed}
+#define TORQUE_2_POSITION {Motor_Ctrl_Type_e::Torque, Motor_Ctrl_Type_e::Position}
+#define DIRECT_POSITION {Motor_Ctrl_Type_e::Position, Motor_Ctrl_Type_e::Position}
+
+RMD_L_40xx_v3<1> CFRMotor(TORQUE_2_SPEED, wheelControllers[0], 0x242);
+RMD_L_40xx_v3<1> CFLMotor(TORQUE_2_SPEED, wheelControllers[1], 0x244);
+RMD_L_40xx_v3<1> CBLMotor(TORQUE_2_SPEED, wheelControllers[2], 0x246);
+RMD_L_40xx_v3<1> CBRMotor(TORQUE_2_SPEED, wheelControllers[3], 0x248);
+
+RMD_L_40xx_v3<1> SFRMotor(DIRECT_POSITION, swerveControllers[0], 0x241);
+RMD_L_40xx_v3<1> SFLMotor(DIRECT_POSITION, swerveControllers[1], 0x243);
+RMD_L_40xx_v3<1> SBLMotor(DIRECT_POSITION, swerveControllers[2], 0x245);
+RMD_L_40xx_v3<1> SBRMotor(DIRECT_POSITION, swerveControllers[3], 0x247);*/
 
 // 首先调取底盘类的构建器，然后使用提供的电机添加函数，将上文构建的电机指针传入构建器，最后由构建器返回构建好的底盘类对象
 Chassis chassis = Chassis::Build().
@@ -96,62 +95,22 @@ Chassis chassis = Chassis::Build().
                   AddSBRMotor(SBRMotor).
                   Build();
 
-
-
-
-/**
- * @brief 底盘根据遥控器数据运动
- */
-
-FZMotion motion;
 RadioMaster_Zorro remote;
-RoutePlanning route_planning(0.3);//Kp为误差补偿系数
-
-float plannedVelX{0},plannedVelY{0},plannedVelAngle{0},plannedX,plannedY,plannedAngle;
-bool isPlanStart = false;
 
 void Task3() {
-    constexpr float speedLimit = 2;
+    constexpr float SPEED_LIMIT = 2.0f;
 
-    if(remote.GetInfo().sC == RemoteControl::SWITCH_STATE_E::DOWN_POS)//任务模式
-    {
-        route_planning.Update(chassis.chassisPos[0][0],chassis.WCSVelocity[0][0],chassis.chassisPos[1][0],chassis.WCSVelocity[1][0],chassis.chassisPos[2][0],chassis.WCSVelocity[2][0]);
-        if(!isPlanStart)
-        {
-            route_planning.AddTarget(0.5,0,0.7,0,-PI/2,0,5);
-            route_planning.AddTarget(1,0,1.5,0,0,0,5);
-            route_planning.AddTarget(0,0,2,0,PI/2,0,5);
-            route_planning.AddTarget(-0.5,0,1,0,PI/4,0,5);
-            route_planning.AddTarget(-1,0,0,0,0,0,5);
-
-			      isPlanStart=true;
-        }
-        route_planning.CalcSpeed();
-        chassis.ChassisSetVelocity(route_planning.FBVel,route_planning.LRVel,route_planning.RTVel);
-        route_planning.GetPlannedVel(plannedX,plannedY,plannedAngle,plannedVelX,plannedVelY,plannedVelAngle);
-    }
-    else if(remote.GetInfo().sC == RemoteControl::SWITCH_STATE_E::UP_POS)//遥控模式
-    {
-        chassis.ChassisSetVelocity(remote.GetInfo().rightCol * speedLimit,
-                                   remote.GetInfo().rightRol * speedLimit,
+    if(remote.GetInfo().sC == RemoteControl::SWITCH_STATE_E::UP_POS) { //遥控模式
+        chassis.ChassisSetVelocity(remote.GetInfo().rightCol * SPEED_LIMIT,
+                                   remote.GetInfo().rightRol * SPEED_LIMIT,
                                    -remote.GetInfo().leftRol * PI);//通道值取负，
     }
-
-    //重置里程计和任务
-    if (remote.GetInfo().sA == RemoteControl::SWITCH_STATE_E::DOWN_POS) {
-        chassis.ResetOdometry(0,0,0);
-        isPlanStart = false;
-    }
-
-    if (remote.GetInfo().sD == RemoteControl::SWITCH_STATE_E::DOWN_POS) {
-      chassis.ResetOdometry(0,0,motion.GetData().rig0_info.eul_Y/180.0f*PI);
-    }
 }
-
 
 /*****  示例4 *****/
 
 void Task4() {
+
 }
 
 /**
@@ -167,11 +126,6 @@ void Setup() {
         remote.Decode(data, length);
     };
     UARTBaseLite<3>::GetInstance().Bind(remoteDecodeFunc);
-
-    std::function<void(uint8_t *, uint16_t)> decodeFunc = [](uint8_t* data, uint16_t length){
-        motion.Decode(data, length);
-    };
-    UARTBaseLite<5>::GetInstance().Bind(decodeFunc);
 }
 
 /**
